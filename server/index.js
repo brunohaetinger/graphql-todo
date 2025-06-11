b08886b0fb86e23bd20a4e1159ab8a35eb766b07
Bruno Haetinger <brunohaetinger@gmail.com>
2025-06-11 00:20:12 -0300

    Add graphQL context for authorization

--- server/index.js
+++ server/index.js
@@ -3,16 +3,38 @@ import { ApolloServer, gql } from "apollo-server";
 const typeDefs = gql`
     type Query {
         hello: String
+        secretCompanyProfit: String
     }
 `;
 
 const resolvers = {
     Query: {
-        hello: () => "Hello GraphQL !"
+        hello: () => "Hello GraphQL !",
+        secretCompanyProfit: (_,__,context) => {
+            if(!context.user) throw new Error('Not Authorized');
+            return '$ 999.999';
+        }
     },
 };
 
-const server = new ApolloServer({typeDefs, resolvers});
+// TODO: to be implemented
+const getUserFromToken = (token) => {
+    if(!token) return null;
+    return {
+        name: 'Steve Gates',
+        age: '50',
+    }
+}
+
+const server = new ApolloServer({
+    typeDefs, 
+    resolvers,
+    context: (({req})=> {
+        const token = req.headers.authorization || '';
+        const user = getUserFromToken(token);
+        return {user};
+    }),
+});
 
 server.listen().then(({url}) => {
     console.log(`🚀 Server ready at ${url}`);
