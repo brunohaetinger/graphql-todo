7327dd8e360672a30a975ed0c1e7f1a8d680fb30
Bruno Haetinger <brunohaetinger@gmail.com>
2025-06-11 00:27:54 -0300

    Add schema

---
+++ server/schema.js
@@ -0,0 +1,35 @@
+// server/schema.js
+import { gql } from 'apollo-server';
+
+const typeDefs = gql`
+  type User {
+    id: ID!
+    email: String!
+  }
+
+  type Task {
+    id: ID!
+    title: String!
+    done: Boolean!
+    userId: ID!
+  }
+
+  type AuthPayload {
+    token: String!
+    user: User!
+  }
+
+  type Query {
+    me: User
+    myTasks: [Task!]!
+  }
+
+  type Mutation {
+    register(email: String!, password: String!): AuthPayload
+    login(email: String!, password: String!): AuthPayload
+    createTask(title: String!): Task!
+    deleteTask(id: ID!): Boolean!
+  }
+`;
+
+export default typeDefs;
