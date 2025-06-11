e5579c8e9f22bffe197979f6684c8f5c5c961cc0
Bruno Haetinger <brunohaetinger@gmail.com>
2025-06-11 00:24:19 -0300

    Add server auth module

---
+++ server/auth.js
@@ -0,0 +1,17 @@
+import jwt from'jsonwebtoken';
+
+const generateToken = (user) => {
+  return jwt.sign({ id: user.id, email: user.email }, process.env.SECRET, { expiresIn: '1h' });
+}
+
+const getUserFromToken = (token) => {
+  try {
+    if (!token) return null;
+    const decoded = jwt.verify(token.replace("Bearer ", ""), process.env.SECRET);
+    return decoded;
+  } catch (err) {
+    return null;
+  }
+}
+
+export default { generateToken, getUserFromToken };
