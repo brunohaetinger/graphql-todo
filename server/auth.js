51a07628296021664fb407234a52a08ed639558c
Bruno Haetinger <brunohaetinger@gmail.com>
2025-06-13 23:34:13 -0300

    Add secrets from dotenv

--- server/auth.js
+++ server/auth.js
@@ -1,13 +1,16 @@
 import jwt from'jsonwebtoken';
+import  'dotenv/config';
+
+const secret = process.env.SECRET;
 
 const generateToken = (user) => {
-  return jwt.sign({ id: user.id, email: user.email }, process.env.SECRET, { expiresIn: '1h' });
+  return jwt.sign({ id: user.id, email: user.email }, secret, { expiresIn: '1h' });
 }
 
 const getUserFromToken = (token) => {
   try {
     if (!token) return null;
-    const decoded = jwt.verify(token.replace("Bearer ", ""), process.env.SECRET);
+    const decoded = jwt.verify(token.replace("Bearer ", ""), secret);
     return decoded;
   } catch (err) {
     return null;
