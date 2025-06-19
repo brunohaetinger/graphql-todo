1e66b3ccd0d3c2475309296799ae31b585f7c0fe
Bruno Haetinger <brunohaetinger@gmail.com>
2025-06-18 23:48:56 -0300

    Client: Fix 404 not found on requests

--- client/src/apollo.ts
+++ client/src/apollo.ts
@@ -1,7 +1,7 @@
 import {ApolloClient, InMemoryCache, createHttpLink} from '@apollo/client';
 import {setContext} from '@apollo/client/link/context';
 
-const httpLink = createHttpLink({uri: 'http://localhost:4000/'})
+const httpLink = createHttpLink({uri: 'http://localhost:4000/graphql'})
 
 const authLink = setContext((_, {headers}) => {
     const token = localStorage.getItem('token');
