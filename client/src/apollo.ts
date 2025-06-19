0037304ac94ca847c5578a1834ba7a39b0392a39
Bruno Haetinger <brunohaetinger@gmail.com>
2025-06-19 00:08:53 -0300

    Client: Update apollo to handle WS or HTTP

--- client/src/apollo.ts
+++ client/src/apollo.ts
@@ -1,19 +1,31 @@
-import {ApolloClient, InMemoryCache, createHttpLink} from '@apollo/client';
+import {ApolloClient, HttpLink, InMemoryCache, split} from '@apollo/client';
 import {setContext} from '@apollo/client/link/context';
+import {WebSocketLink} from '@apollo/client/link/ws';
+import { getMainDefinition } from '@apollo/client/utilities';
 
-const httpLink = createHttpLink({uri: 'http://localhost:4000/graphql'})
+const httpLink = new HttpLink({uri: 'http://localhost:4000/graphql'})
 
-const authLink = setContext((_, {headers}) => {
-    const token = localStorage.getItem('token');
-    return {
-        headers: {
-            ...headers,
-            authorization: token ? `Bearer ${token}` : '',
+const wsLink = new WebSocketLink({
+    uri: `ws://localhost:4000/graphql`,
+    options: {
+        reconnect: true,
+        connectionParams: {
+            authToken: localStorage.getItem('token'),
         }
     }
-});
+})
+
+// Route to ws or http depending on the operation
+const splitLink = split(
+    ({query}) => {
+        const def = getMainDefinition(query);
+        return def.kind === 'OperationDefinition' && def.operation === 'subscription';
+    },
+    wsLink,
+    httpLink,
+);
 
 export const client = new ApolloClient({
-    link: authLink.concat(httpLink),
+    link: splitLink,
     cache: new InMemoryCache(),
 })
