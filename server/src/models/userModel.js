06343ce16ddc177bdfc7ae85e15eb1d27322eb6f
Bruno Haetinger <brunohaetinger@gmail.com>
2025-06-27 15:47:50 -0300

    Server: remove old in-memory models

--- server/src/models/userModel.js
+++
@@ -1,8 +0,0 @@
-const users = [];
-
-export default {
-  getAllUsers: () => users,
-  getUserByEmail: (email) => users.find(u => u.email === email),
-  getUserById: (id) => users.find(u => u.id === id),
-  addUser: (user) => users.push(user)
-};
