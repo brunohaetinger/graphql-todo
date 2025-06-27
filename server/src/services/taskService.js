d052cb3738b181e64c06bc5e7eda0cd1b626d8c8
Bruno Haetinger <brunohaetinger@gmail.com>
2025-06-27 15:54:45 -0300

    Server: use db on taskService

--- server/src/services/taskService.js
+++ server/src/services/taskService.js
@@ -1,16 +1,18 @@
 import { v4 as uuidv4 } from 'uuid';
-import taskModel from '../models/taskModel.js';
+import db from '../models/index.js';
 import { pubsub, TASK_CREATED } from '../auth/pubsub.js';
 
 export default {
-  createTask: (title, user) => {
-    const task = { id: uuidv4(), title, done: false, userId: user.id };
-    taskModel.addTask(task);
+  createTask: async (title, user) => {
+    const task = await db.Task.create({ title, done: false, userId: user.id });
     pubsub.publish(TASK_CREATED, { taskCreated: task });
     return task;
   },
 
-  getMyTasks: (user) => taskModel.getTasksByUser(user.id),
+  getMyTasks: async(user) => await db.Task.findAll({where: {userId: user.id}}),
 
-  deleteTask: (id, user) => taskModel.deleteTaskById(id, user.id)
+  deleteTask: async (id, user) => {
+    const deleted = await db.Task.destroy({where: { id, userId: user.id }});
+    return deleted > 0;
+  },
 };
